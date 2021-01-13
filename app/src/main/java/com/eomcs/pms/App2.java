9c636c83aaa560a9935aa55d46aaec3ff00365cc
leeheejin <leeheejin@leeheejinui-MacBookPro.local>
2021-01-13 13:34:18 +0900

    v02

---
+++ app/src/main/java/com/eomcs/pms/App2.java
@@ -0,0 +1,16 @@
+package com.eomcs.pms;
+
+public class App2 {
+  public static void main(String[] args) {
+    System.out.print("[");
+    System.out.print("프로젝트");
+    System.out.println("]");
+    System.out.printf("번호: %d\n",1201);
+    System.out.printf("프로젝트명: %s\n","미니 프로젝트 관리 시스템 개발");
+    System.out.printf("내용: %s\n", "소규모 팀을 위한 프로젝트 관리 시스템을 개발한다.");
+    System.out.printf("시작일: %d\n", 2020-01-01);
+    System.out.printf("종료일: %d\n", 2020-12-31);
+    System.out.printf("만든이: %s\n","홍길동");
+    System.out.printf("팀원: %s\n%s\n", "홍길동,김구,유관순,안중근,윤봉길","```");
+  }
+}
---
+++ app/src/main/java/com/eomcs/pms/App3.java
@@ -0,0 +1,14 @@
+package com.eomcs.pms;
+
+public class App3 {
+  public static void main(String[] args) {
+    System.out.println("[작업]\n"
+        + "프로젝트: 미니 프로젝트 관리 시스템 개발\n"
+        + "번호: 1\n"
+        + "내용: 요구사항 수집\n"
+        + "완료일: 2020-01-20\n"
+        + "상태: 진행중\n"
+        + "담당자: 홍길동\n"
+        + "```");
+  }
+}

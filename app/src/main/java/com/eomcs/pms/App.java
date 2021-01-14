8a80dd9043f318a3f422f9a0e2f1cc2d7063244a
leeheejin <leeheejin@leeheejinui-MacBookPro.local>
2021-01-14 11:29:36 +0900

    .

--- app/src/main/java/com/eomcs/pms/App.java
+++ app/src/main/java/com/eomcs/pms/App.java
@@ -6,28 +6,28 @@ public class App {
     java.util.Date today = new java.util.Date();
 
     System.out.println("[회원]");
-    System.out.println("번호? ");
-    int id = keyboard.nextInt();
+    System.out.print("번호? ");
+    String id = keyboard.nextLine();
     keyboard.nextLine();
-    System.out.println("이름? ");
+    System.out.print("이름? ");
     String name = keyboard.nextLine();
-    System.out.println("이메일? ");
+    System.out.print("이메일? ");
     String email = keyboard.nextLine();
-    System.out.println("암호? ");
+    System.out.print("암호? ");
     int pw = keyboard.nextInt();
     keyboard.nextLine();
-    System.out.println("사진? ");
+    System.out.print("사진? ");
     String photo = keyboard.nextLine();
-    System.out.println("전화? ");
-    String phoneNum = keyboard.nextLine();
+    System.out.print("전화? ");
+    String tel = keyboard.nextLine();
     keyboard.close();
     System.out.println("----------------------------");
-    System.out.printf("번호: %d\n", id);
+    System.out.printf("번호: s\n", id);
     System.out.printf("이름: %s\n", name);
     System.out.printf("이메일: %s\n", email);
     System.out.printf("암호: %d\n", pw);
     System.out.printf("사진: %s\n", photo);
-    System.out.printf("전화: %s\n", phoneNum);
+    System.out.printf("전화: %s\n", tel);
     System.out.printf("가입일: %1$tY-%1$tm-%1$td", today);
 
   }
--- app/src/main/java/com/eomcs/pms/App2.java
+++ app/src/main/java/com/eomcs/pms/App2.java
@@ -18,9 +18,9 @@ public class App2 {
     System.out.print("종료일? ");
     String endDate = keyboard.nextLine();
     System.out.print("만든이? ");
-    String madeBy = keyboard.nextLine();
+    String owner = keyboard.nextLine();
     System.out.print("팀원? ");
-    String member = keyboard.nextLine();
+    String members = keyboard.nextLine();
     keyboard.close();
     System.out.println("----------------------------");
     System.out.printf("번호: %d\n", id);
@@ -28,7 +28,7 @@ public class App2 {
     System.out.printf("내용: %s\n", content);
     System.out.printf("시작일: %s\n", startDate);
     System.out.printf("종료일: %s\n", endDate);
-    System.out.printf("만든이: %s\n", madeBy);
-    System.out.printf("팀원: %s\n", member);
+    System.out.printf("만든이: %s\n", owner);
+    System.out.printf("팀원: %s\n", members);
   }
 }

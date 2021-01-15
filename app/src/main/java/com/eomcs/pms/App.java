6cc5248dacd7deaf6890b0d5e79bb128b54ca9cb
leeheejin <leeheejin@leeheejinui-MacBookPro.local>
2021-01-15 16:00:40 +0900

    .

--- app/src/main/java/com/eomcs/pms/App.java
+++ app/src/main/java/com/eomcs/pms/App.java
@@ -1,59 +1,80 @@
 package com.eomcs.pms;
 
+import java.sql.Date;
+import java.util.Scanner;
+
+// 1) 낱개의 변수를 사용하여 여러 회원 정보 처리하기
+// 2) 조건문을 사용하여 입출력 제어하기
+// - 필요한 만큼만 입력 받고 출력하고 싶다.
+// 3) 배열을 사용하여 여러 개의 값을 다루기
+// - 배열을 사용하면 간단하게 여러 개의 변수를 선언할 수 있다.
+// 4) 반복문을 사용하여 여러 개의 값을 다루기
+// - 반복문을 사용하면 같은 코드를 중복해서 작성할 필요가 없다.
+// 5) 배열 개수를 변수에서 관리하기
+// - 변수의 값만 바꾸면 배열 개수를 바로 변경할 수 있어 편하다.
+// 6) 상수를 사용하여 초기 값을 변경하지 못하게 막기
+// - 변수는 중간에 값을 바꿀 수 있기 때문에 값을 바꾸지 말아야 하는 경우
+//   상수로 선언한다.
 public class App {
+
   public static void main(String[] args) {
-    java.util.Scanner keyboard = new java.util.Scanner(System.in);
-    java.util.Date today = new java.util.Date();
-
-    final int SIZE = 5;
-
-    int[] ids = new int[SIZE];
-    String[] names = new String[SIZE];
-    String[] emails = new String[SIZE];
-    int[] pws = new int[SIZE];
-    String[] photos = new String[SIZE];
-    String[] tels = new String[SIZE];
-    java.sql.Date[] nows = new java.sql.Date[SIZE];
-
     System.out.println("[회원]");
 
-    for (int i = 0; i < SIZE; i++) {
-      System.out.print("번호? ");
-      ids[i] = keyboard.nextInt();
+    Scanner keyboardScan = new Scanner(System.in);
 
-      keyboard.nextLine();
+    // 최대 입력 개수
+    final int LENGTH = 100;
+
+    int[] no = new int[LENGTH];
+    String[] name = new String[LENGTH];
+    String[] email = new String[LENGTH];
+    String[] password = new String[LENGTH];
+    String[] photo = new String[LENGTH];
+    String[] tel = new String[LENGTH];
+    Date[] registeredDate = new Date[LENGTH];
+
+    int size = 0;
+
+    for (int i = 0; i < LENGTH; i++) {
+      System.out.print("번호? ");
+      no[i] = Integer.parseInt(keyboardScan.nextLine());
+
       System.out.print("이름? ");
-      names[i] = keyboard.nextLine();
+      name[i] = keyboardScan.nextLine();
 
       System.out.print("이메일? ");
-      emails[i] = keyboard.nextLine();
+      email[i] = keyboardScan.nextLine();
 
       System.out.print("암호? ");
-      pws[i] = keyboard.nextInt();
+      password[i] = keyboardScan.nextLine();
 
-      keyboard.nextLine();
       System.out.print("사진? ");
-      photos[i] = keyboard.nextLine();
+      photo[i] = keyboardScan.nextLine();
 
       System.out.print("전화? ");
-      tels[i] = keyboard.nextLine();
+      tel[i] = keyboardScan.nextLine();
 
-      nows[i] = new java.sql.Date(System.currentTimeMillis());
+      registeredDate[i] = new java.sql.Date(System.currentTimeMillis());
 
-      System.out.println();
+      size++;
+      System.out.println(); // 빈 줄 출력
+
       System.out.print("계속 입력하시겠습니까?(y/N) ");
-      String response = keyboard.nextLine();
-      if(response == null)
+      String str = keyboardScan.nextLine();
+      if (!str.equalsIgnoreCase("y")) {
         break;
-
-    }
-    keyboard.close();
-    System.out.println("----------------------------------");
-    for (int i = 0; i < SIZE; i++) {
-      System.out.printf("%d, %s, %s, %d, %s, %s, %s\n", 
-          ids[i], names[i], emails[i], pws[i], photos[i], tels[i], nows[i]);
+      }
+      System.out.println(); // 빈 줄 출력
     }
 
+    keyboardScan.close();
 
+    System.out.println("--------------------------------");
+
+    for (int i = 0; i < size; i++) {
+      // 번호, 이름, 이메일, 전화, 가입일
+      System.out.printf("%d, %s, %s, %s, %s\n", // 출력 형식 지정
+          no[i], name[i], email[i], tel[i], registeredDate[i]);
+    }
   }
 }
--- app/src/main/java/com/eomcs/pms/App2.java
+++ app/src/main/java/com/eomcs/pms/App2.java
@@ -1,34 +1,66 @@
 package com.eomcs.pms;
 
+import java.sql.Date;
+
 public class App2 {
   public static void main(String[] args) {
     java.util.Scanner keyboard = new java.util.Scanner(System.in);
 
+    final int SIZE = 5;
+
+    int[] ids = new int[SIZE];
+    String[] titles = new String[SIZE];
+    String[] contents = new String[SIZE];
+    Date[] startDates = new Date[SIZE];
+    Date[] endDates = new Date[SIZE];
+    String[] owners = new String[SIZE];
+    String[] teams = new String[SIZE];
+
+    int count = 0;
 
     System.out.println("[프로젝트]");
-    System.out.print("번호?");
-    int id = keyboard.nextInt();
-    keyboard.nextLine();
-    System.out.print("프로젝트명? ");
-    String title = keyboard.nextLine();
-    System.out.print("내용? ");
-    String content = keyboard.nextLine();
-    System.out.print("시작일? ");
-    String startDate = keyboard.nextLine();
-    System.out.print("종료일? ");
-    String endDate = keyboard.nextLine();
-    System.out.print("만든이? ");
-    String owner = keyboard.nextLine();
-    System.out.print("팀원? ");
-    String members = keyboard.nextLine();
+
+    for (int i = 0; i < SIZE; i++) {
+      System.out.print("번호? ");
+      ids[i] = keyboard.nextInt();
+
+      keyboard.nextLine();
+      System.out.print("프로젝트명? ");
+      titles[i] = keyboard.nextLine();
+
+      System.out.print("내용? ");
+      contents[i] = keyboard.nextLine();
+
+      System.out.print("시작일? ");
+      startDates[i] = Date.valueOf(keyboard.nextLine());
+
+      System.out.print("종료일? ");
+      endDates[i] = Date.valueOf(keyboard.nextLine());
+
+      System.out.print("만든이? ");
+      owners[i] = keyboard.nextLine();
+
+      System.out.print("팀원? ");
+      teams[i] = keyboard.nextLine();
+
+      count++;
+
+      System.out.println();
+      System.out.print("계속 입력하시겠습니까?(y/N) ");
+      String response = keyboard.nextLine();
+      if (response.length() == 0 | response.equalsIgnoreCase("n") | response.equalsIgnoreCase("N")) {
+        break;
+      }
+      System.out.println();
+
+
+    }
     keyboard.close();
-    System.out.println("----------------------------");
-    System.out.printf("번호: %d\n", id);
-    System.out.printf("프로젝트명: %s\n", title);
-    System.out.printf("내용: %s\n", content);
-    System.out.printf("시작일: %s\n", startDate);
-    System.out.printf("종료일: %s\n", endDate);
-    System.out.printf("만든이: %s\n", owner);
-    System.out.printf("팀원: %s\n", members);
+    System.out.println("------------------------------------");
+
+    for (int i = 0; i < count; i++) {
+      System.out.printf("%d, %s, %s, %s, %s, %s, %s\n", 
+          ids[i], titles[i], contents[i], startDates[i], endDates[i], owners[i], teams[i]);
+    }
   }
 }

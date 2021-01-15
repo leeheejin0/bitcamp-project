a7ac775d11c600ba78a62132ae9a790347c666e3
leeheejin <leeheejin@leeheejinui-MacBookPro.local>
2021-01-15 14:48:38 +0900

    .

--- app/src/main/java/com/eomcs/pms/App.java
+++ app/src/main/java/com/eomcs/pms/App.java
@@ -5,30 +5,55 @@ public class App {
     java.util.Scanner keyboard = new java.util.Scanner(System.in);
     java.util.Date today = new java.util.Date();
 
+    final int SIZE = 5;
+
+    int[] ids = new int[SIZE];
+    String[] names = new String[SIZE];
+    String[] emails = new String[SIZE];
+    int[] pws = new int[SIZE];
+    String[] photos = new String[SIZE];
+    String[] tels = new String[SIZE];
+    java.sql.Date[] nows = new java.sql.Date[SIZE];
+
     System.out.println("[회원]");
-    System.out.print("번호? ");
-    String id = keyboard.nextLine();
-    keyboard.nextLine();
-    System.out.print("이름? ");
-    String name = keyboard.nextLine();
-    System.out.print("이메일? ");
-    String email = keyboard.nextLine();
-    System.out.print("암호? ");
-    int pw = keyboard.nextInt();
-    keyboard.nextLine();
-    System.out.print("사진? ");
-    String photo = keyboard.nextLine();
-    System.out.print("전화? ");
-    String tel = keyboard.nextLine();
+
+    for (int i = 0; i < SIZE; i++) {
+      System.out.print("번호? ");
+      ids[i] = keyboard.nextInt();
+
+      keyboard.nextLine();
+      System.out.print("이름? ");
+      names[i] = keyboard.nextLine();
+
+      System.out.print("이메일? ");
+      emails[i] = keyboard.nextLine();
+
+      System.out.print("암호? ");
+      pws[i] = keyboard.nextInt();
+
+      keyboard.nextLine();
+      System.out.print("사진? ");
+      photos[i] = keyboard.nextLine();
+
+      System.out.print("전화? ");
+      tels[i] = keyboard.nextLine();
+
+      nows[i] = new java.sql.Date(System.currentTimeMillis());
+
+      System.out.println();
+      System.out.print("계속 입력하시겠습니까?(y/N) ");
+      String response = keyboard.nextLine();
+      if(response == null)
+        break;
+
+    }
     keyboard.close();
-    System.out.println("----------------------------");
-    System.out.printf("번호: s\n", id);
-    System.out.printf("이름: %s\n", name);
-    System.out.printf("이메일: %s\n", email);
-    System.out.printf("암호: %d\n", pw);
-    System.out.printf("사진: %s\n", photo);
-    System.out.printf("전화: %s\n", tel);
-    System.out.printf("가입일: %1$tY-%1$tm-%1$td", today);
+    System.out.println("----------------------------------");
+    for (int i = 0; i < SIZE; i++) {
+      System.out.printf("%d, %s, %s, %d, %s, %s, %s\n", 
+          ids[i], names[i], emails[i], pws[i], photos[i], tels[i], nows[i]);
+    }
+
 
   }
 }
